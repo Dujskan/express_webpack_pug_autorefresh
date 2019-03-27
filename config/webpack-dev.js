95bceaddd40d384dc1d368643f1f0777be63e96d
dujskan <sebbelundin@hotmail.com>
2019-03-27 20:59:22 +0100

    added file-loader to support images

--- config/webpack-dev.js
+++ config/webpack-dev.js
@@ -5,33 +5,43 @@ const path = require('path')
 const rootConfig = require('../root_config')
 
 module.exports = {
-  entry: path.join(rootConfig.clientPath, 'app.js'),
-  output: {
-    filename: '[name]-bundle.js',
-    path: path.join(rootConfig.rootPath, 'dist')
-  },
-  mode: 'development',
-  devServer: {
-    contentBase: "dist",
-    overlay: false,
-    stats: {
-        colors: true
-    }
-  },
+    entry: path.join(rootConfig.clientPath, 'app.js'),
+    output: {
+        filename: '[name]-bundle.js',
+        path: path.join(rootConfig.rootPath, 'dist')
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: "dist",
+        overlay: false,
+        stats: {
+            colors: true
+        }
+    },
     module: {
         rules: [{
-            test: /\.sass$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                {
-                    loader: "sass-loader",
-                    options: {
-                        sourceMap: true
+                test: /\.sass$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true
+                        }
                     }
-                }
-            ]
-        }]
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    },
+                }, ],
+            },
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
